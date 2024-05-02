a2a6386029b573cc9be79dcecc1feb4569a50f03
galang48 <galangyudha1945@gmail.com>
2024-05-02 11:25:59 +0700

    menerapkan penyisihan daftar tetap

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -15,9 +15,19 @@ Node *START = NULL;
 void addNode() {
     Node* newNode = new Node(); // step 1: create a new mode
     cout << "\nEnter the role number of the student: ";
-    cin >> newNode->NoMhs;      // Assign value to the data field of the new node
+    cin >> newNode->noMhs;      // Assign value to the data field of the new node
     cout << "\nEnter the name of the student: ";
     cin >> newNode->name;       // Assign value to the data field of the new mode
+
+
+    // Insert the new mode in the list
+    if (START == NULL || newNode->noMhs <= START->noMhs) {
+        
+        if (START != NULL && newNode->noMhs == START->noMhs) {
+            cout << "\033[31mDuplicate roll numbers not allowed\033[0m" << endl;
+            return;
+        }
+    }
 }
 int main()
 {
