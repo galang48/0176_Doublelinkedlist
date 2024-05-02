7b96c8f0cdeb8f7375370e65f34e475d4aed5cf6
galang48 <galangyudha1945@gmail.com>
2024-05-02 11:32:30 +0700

    membuat first node pada new node

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -29,6 +29,9 @@ void addNode() {
         }
         // if the list is empty, make the new node the START
         newNode->next = START; // step 3: make the new node point to the first node
+        if (START != NULL) {
+            START->prev = newNode; // step 4: male the first node point to the new node
+        }
     }
 }
 int main()
