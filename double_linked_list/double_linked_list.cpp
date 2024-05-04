01a128de738333f444fa6db9c30bd3bff8095db5
galang48 <galangyudha1945@gmail.com>
2024-05-04 20:12:02 +0700

    jika previous masih null itu berarti newnode menjadi START

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -55,6 +55,10 @@ void addNode() {
         if (previous != NULL) {
             previous->next = newNode;   //step 7: make the next field of the previous node
         }
+        else {
+            //if previous is still NULL, it means newNode is now the first node
+            START = newNode;
+        }
     }
 }
 
